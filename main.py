8040e615f0f4d0908bc3773b68e68f78c10c81f4
dkalaitzi <despoinakalaitzi@gmail.com>
2023-03-16 20:34:32 +0200

    Plots

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-df=pd.read_csv("final.csv")
+data=pd.read_csv("final.csv")
+q=data.groupby('zip_code').bottles_sold.max()
+#n = df.groupby('zip_code')
+#n=cn.agg({'bottles_sold':max})
+f1 = plt.figure(1)
+plt.scatter(q.index,q,c=np.random.rand(len(q),3), s=400)
+plt.xlabel('Zip Code')
+plt.ylabel('Bottles Sold')
+plt.title('Bottles Sold')
+#plt.show()
+q2=data.groupby('store_number').bottles_sold.sum().sort_values()
+f2 = plt.figure(2)
+q2.plot(kind='barh')
+plt.xlabel('Sales per Store')
+plt.ylabel('Store Number')
+plt.title('Sales per Store')
+plt.show()
 
-cn = df.groupby('zip_code')
-print(cn.agg({'bottles_sold':max}))
