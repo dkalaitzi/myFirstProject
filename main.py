3702d864bab044b6e92aa5041eccf01a80961478
dkalaitzi <despoinakalaitzi@gmail.com>
2023-03-21 01:01:19 +0200

    Final commit with percentage

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data=pd.read_csv("final.csv")
+#Figure 1: most popular item per zipcode
 q=data.groupby('zip_code').bottles_sold.max()
-#n = df.groupby('zip_code')
-#n=cn.agg({'bottles_sold':max})
+
 f1 = plt.figure(1)
 plt.scatter(q.index,q,c=np.random.rand(len(q),3), s=400)
 plt.xlabel('Zip Code')
 plt.ylabel('Bottles Sold')
 plt.title('Bottles Sold')
-#plt.show()
+
+#Figure 2: percentage of sales per store
 q2=data.groupby('store_number').bottles_sold.sum().sort_values()
+q2 = pd.DataFrame(q2, columns=['store_number','bottles_sold'])
+# Computing Percentage
+q2['Percent of Sales %'] = (q2['bottles_sold'] /
+                  q2['bottles_sold'].sum()) * 100
+s = pd.DataFrame(q2, columns=['Percent of Sales %'])
+#plot
+s.plot(kind='barh')
 f2 = plt.figure(2)
-q2.plot(kind='barh')
 plt.xlabel('Sales per Store')
 plt.ylabel('Store Number')
-plt.title('Sales per Store')
+plt.title('Sales per Store %')
 plt.show()
+
 
